86e7b5fb06c51899eeba910993b2ab8a882d9d26
vosw1 <vosw1@naver.com>
2023-12-13 11:16:08 +0900

    캐스팅과 상수

---
+++ src/ex02/ConstEx01.java
@@ -0,0 +1,13 @@
+package ex02;
+
+public class ConstEx01 {
+    public static void main(String[] args) {
+        int num = 1; // 변수 : 변하는 값
+        final int sum = 10; // 상수 : 변하지 않는 값
+
+        num = 2;
+        //sum = 20; (변경 불가능)
+
+
+    }
+}
