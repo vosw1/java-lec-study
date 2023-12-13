1cd5dba68d3198319c1eef125287359f2be5d3b4
vosw1 <vosw1@naver.com>
2023-12-13 12:40:25 +0900

    ãstatic

---
+++ src/ex02/IdenEx01.java
@@ -0,0 +1,20 @@
+package ex02;
+
+/**
+ * 변수 작성법
+ * 1. 카멜 표기법을 사용
+ * 2. 숫자를 먼저 사용하지 않음
+ * 3. 특수문자를 사용하지 않음
+ */
+
+public class IdenEx01 {
+    public static void main(String[] args) {
+       //int class; // 키워드 : 자바가 기본적으로 쓰고 있는 것
+        int abc_; // 절대 사용 안함!
+        int user_money; // 카멜 표기법: 띄워쓰기 시 첫글자 대문자로 표시
+        // int abc#;
+        int method;
+        double abc$$;
+        //int 1stMoney;  숫자로 시작할 수 없음
+    }
+}
---
+++ src/ex04/MemEx01.java
@@ -0,0 +1,20 @@
+package ex04;
+
+/**
+ * JVM이 시작할때
+ * 1. static 찾음 -> age, main
+ * 2. main 찾아서 실행
+ */
+class Person1{
+    static int age = 20;
+    static char gender = '여';
+    // 원시 자료형(primitive data type)
+    // 커스텀 자료형(내가 만든 자료형)
+}
+
+public class MemEx01 {
+    public static void main(String[] args) {
+        System.out.println(Person1.age);
+        System.out.println(Person1.gender);
+    }
+}
