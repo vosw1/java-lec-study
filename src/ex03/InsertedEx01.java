ad7ca8c6843cd3391761f0b775b41e1e48cd4b5c
vosw1 <vosw1@naver.com>
2023-12-18 17:37:17 +0900

    삽입정렬 연습

--- src/ex03/InsertedEx01.java
+++ src/ex03/InsertedEx01.java
@@ -2,6 +2,78 @@ package ex03;
 
 public class InsertedEx01 {
     public static void main(String[] args) {
+        int[] arr = {5, 8, 2, 4, 3};
 
+        int target;
+        int temp;
+
+        // 1회전
+        target = arr[1]; // 8
+        if(arr[0] > arr[1]){
+            temp = arr[0];
+            arr[0] = arr[1];
+            arr[1] = temp;
+        }
+        // 2회전
+        target = arr[2];// 2
+        if(arr[1] > arr[2]){
+            temp = arr[1];
+            arr[1] = arr[2];
+            arr[2] = temp;
+        }
+        target = arr[1];
+        if(arr[0] > arr[1]){
+            temp = arr[0];
+            arr[0] = arr[1];
+            arr[1] = temp;
+        }
+        // 3회전
+        target = arr[3];
+        if(arr[2] > arr[3]) {
+            temp = arr[2];
+            arr[2] = arr[3];
+            arr[3] = temp;
+        }
+        target = arr[2];// 2
+        if(arr[1] > arr[2]){
+            temp = arr[1];
+            arr[1] = arr[2];
+            arr[2] = temp;
+        }
+        target = arr[1];
+        if(arr[0] > arr[1]){
+            temp = arr[0];
+            arr[0] = arr[1];
+            arr[1] = temp;
+        }
+        // 3회전
+        target = arr[4];
+        if(arr[3] > arr[4]) {
+            temp = arr[3];
+            arr[3] = arr[4];
+            arr[4] = temp;
+        }
+
+        target = arr[3];
+        if(arr[2] > arr[3]) {
+            temp = arr[2];
+            arr[2] = arr[3];
+            arr[3] = temp;
+        }
+        target = arr[2];
+        if(arr[1] > arr[2]){
+            temp = arr[1];
+            arr[1] = arr[2];
+            arr[2] = temp;
+        }
+        target = arr[1];
+        if(arr[0] > arr[1]){
+            temp = arr[0];
+            arr[0] = arr[1];
+            arr[1] = temp;
+        }
+        for (int v : arr) { // 전체만 출력 가능
+            System.out.print(v + " ");
+        }
     }
 }
---
+++ src/ex03/InsertedEx02.java
@@ -0,0 +1,28 @@
+package ex03;
+
+public class InsertedEx02 {
+    public static void main(String[] args) {
+        int[] arr = {5, 8, 2, 4, 3};
+
+        int target;
+        int temp;
+        int N = arr.length;
+
+        // 1회전
+        for (int i = 0; i < N-1; i++) {
+
+            for (int j = 1; j < N; j++) {
+                target = arr[j]; // 8
+
+                if (arr[j - 1] > arr[j]) {
+                    temp = arr[j - 1];
+                    arr[j - 1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        for (int v : arr) { // 전체만 출력 가능
+            System.out.print(v + " ");
+        }
+    }
+}
