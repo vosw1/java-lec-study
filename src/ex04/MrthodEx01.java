a8c6ebc9630044db08a984ed7d9b4bd4e47bf351
vosw1 <vosw1@naver.com>
2023-12-20 09:05:21 +0900

    method practice

--- src/ex04/MrthodEx01.java
+++ src/ex04/MrthodEx01.java
@@ -1,22 +1,19 @@
 package ex04;
 
-public class MrthodEx01 {
+public class MethodEx01 {
 
-    static void m1(){
+    static void m1() {
         System.out.println("m1");
     }
 
-    static String m2(){
+    static String m2() {
         System.out.println("m2");
-        return "m2"; // 메서드, 종료
+        return "m2"; // 메서드 종료
     }
 
     public static void main(String[] args) {
-        m1(); // static은 같은 클래스에 있으면 클래스명을 생략 가능함
-        m2(); // 실행시에 내부(10, 11번 라인)이 열림 -> 메모리에 뜸
-              // 실행이 끝나면 리턴 값으로 바뀜
-              // 결과값이 String result = "m2"과 동일
-              // 결과값이 String result = m2()와 동일
-              // 반환되는 값과 타입을 일치시켜줘야 함
+        m1();
+        String result = m2();
+        System.out.println("result : " + result);
     }
 }
