9ecf2c44be98477ad9b27b2ae0c7d66ea392a191
vosw1 <vosw1@naver.com>
2023-12-21 12:02:01 +0900

    êbank app test

--- src/ex04/example/model/Account.java
+++ src/ex04/example/model/Account.java
@@ -1,9 +1,9 @@
 package ex04.example.model;
 
 public class Account{
-    final int id; // 계좌번호 (숫자 4자리로 만들어져있다)
-    long balance; //
-    int userId; // 1:ssar의 계좌 -> 부식별자가 필요함
+    public final int id; // 계좌번호 (숫자 4자리로 만들어져있다)
+    public long balance; //
+    public int userId; // 1:ssar의 계좌 -> 부식별자가 필요함
 
     public Account(int id, long balance, int userId) {
         this.id = id;
