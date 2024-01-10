ffaafe8b75fdbd4f2a3ac384c1d3d1146529e540
vosw1 <vosw1@naver.com>
2024-01-10 16:10:18 +0900

    전이중

--- src/ex17/multi/Server.java
+++ src/ex17/multi/Server.java
@@ -6,65 +6,39 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.Charset;
 import java.util.Scanner;
 
 public class Server {
     public static void main(String[] args) {
         try {
-            //1. 소켓과 버퍼 만들기
-            // main 스레드
-            ServerSocket serverSocket = new ServerSocket(10000); // 리스너 소켓 생성
-            Socket socket = serverSocket.accept(); // 대기중 -> 연결후 랜덤포트 소켓 생성
-            //System.out.println("서버 : 연결 완료");
+            // 1. 소켓과 버퍼 만들기
+            ServerSocket serverSocket = new ServerSocket(20000);
+            Socket socket = serverSocket.accept();
 
-            // 2. 메세지 받기 스레드
-            // receivedThread
-            Thread receivedThread = new Thread(() ->{ // 스택이 달라서 readLine()에서 오류 발생
-                //System.out.println("서버 : 읽기 스레드 생성 완료");
-                try{
-                    // 버퍼 만들기(received)
-                    BufferedReader br = new BufferedReader(
-                            new InputStreamReader(socket.getInputStream())
-                    );
-                    //System.out.println("서버 : 읽기 버퍼 생성 완료");
-                    System.out.println("서버 : 통신 연결 완료");
-                    while(true) {
+            BufferedReader br = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream())
+            );
+
+            // 2. 메시지 받기 스레드
+            new Thread(() -> {
+                while (true) {
+                    try {
                         String requestMsg = br.readLine();
-                        System.out.println("클라이언트로부터 받은 메세지 : " + requestMsg);
-                        //System.out.println("클라이언트로부터 받은 메세지 읽기 완료");
-                        if(requestMsg.equals("끝")) {
-                            System.out.println("서버 : 통신 종료");
-                            break;
-                        }
+                        System.out.println("클라이언트로부터 받은 메시지 : " + requestMsg);
+                    } catch (Exception e) {
+                        e.printStackTrace();
                     }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            });
+                }}).start();
 
-            Thread sendThread = new Thread(() -> {
-                //System.out.println("서버 : 쓰기 스레드 생성 완료");
-                try {
-                    // 버퍼 만들기(send)
-                    PrintWriter pw = new PrintWriter(socket.getOutputStream(), true);
-                    //System.out.println("서버 : 쓰기 버퍼 생성 완료");
-                    while (true) {
-                        Scanner sc = new Scanner(System.in);
-                        String sendMsg = sc.nextLine();
-                        pw.println(sendMsg);
-                        //System.out.println("서버 : 메세지 키보드로 받아서 보내기 완료");
-                        if(sendMsg.equals("끝")) {
-                            System.out.println("서버 : 통신 종료");
-                            break;
-                        }
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
+            PrintWriter pw = new PrintWriter(socket.getOutputStream(), true);
+            Scanner sc = new Scanner(System.in);
+            new Thread(() -> {
+                while (true) {
+                    String keyboardMsg = sc.nextLine();
+                    pw.println(keyboardMsg);
                 }
-            });
-
-            receivedThread.start();
-            sendThread.start();
+            }).start();
 
         } catch (IOException e) {
             throw new RuntimeException(e);
